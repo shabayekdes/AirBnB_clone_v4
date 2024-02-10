f3ce605d371d87733c993ff5519a5be57b3b073a
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-02-10 19:49:08 +0200

    Add task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,14 +5,14 @@ from models import storage
 from os import getenv
 from api.v1.views import app_views
 from flask_cors import CORS
-
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
@@ -21,9 +21,21 @@ def teardown_appcontext(exception):
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
 if __name__ == "__main__":
